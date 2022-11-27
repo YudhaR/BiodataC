8fb44674d6ffadd74c29b904ebd6349d16098e66
YudhaR <yudharizqia182@gmail.com>
2022-11-28 04:25:41 +0700

    first commit

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("BIODATA \nNama : Yudha Rizqia Grafer \nNIM  : A11.2022.14280 \nTTL  : Tegal 6 Juli 2004 \nHobi : Rebahan");
+    return 0;
+}
